2faa267a6ec9b56f12f7e1242c6cb87972561a5b
Marcin Karliński <marcin.karlinski5@gmail.com>
2022-06-06 14:28:31 +0200

    created a joint R file for both logistic regression and random forest

---
+++ R_full_script.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(caret)
+library(car)
+library(caTools)
+library(corrplot)
+library(InformationValue)
+library(randomForest)
+
+pulsar_stars <- read.csv("pulsar_stars.csv")
